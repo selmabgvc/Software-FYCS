066839360ae98d06adae531f97598e9c31bb9dce
claudia-wrobel <claudia.wrobel00@gmail.com>
2025-12-06 14:34:18 +0100

    git push

--- repeat_the_colors.py
+++ repeat_the_colors.py
@@ -6,3 +6,7 @@ colors = ["red", "blue", "green"]
 
 print ("Original list:", colors)
 repeated_colors = colors * 3
+
+print("Repeated list:", repeated_colors)
+# This code repeats the colors in the list three times and prints the original and repeated lists.
+print("End of the program")
